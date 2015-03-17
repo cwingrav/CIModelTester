93d99e62437198d8d82bf998432460852e57b6bb
Chad Wingrave <cwingrav@Chadwicks-MBP.home>
2015-03-17 12:47:34 -0400

    Updates.

--- modeltester.php
+++ modeltester.php
@@ -1,156 +1,156 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
 class ModelTester extends CI_Controller {
 
-    public function __construct($_mymodels) {
+    public function __construct($_mymodels,$_isvalid = true) {
         parent::__construct();
-        $this->load->library('session');
-        $this->load->helper('form');
-        $this->load->helper('url');
-        $this->load->helper('html');
-        $this->load->database();
-        $this->load->library('form_validation');
-        //$this->load->library('template');
 
-		$this->mymodels = $_mymodels;//array("adder_model","login_model","recdata_model","admin_model","cs_model","report_model","rec_model");
+		$this->isvalid = false;
+		if ( ENVIRONMENT == 'development' ) {
+			$this->isvalid = $_isvalid;
+			$this->load->library('session');
+			$this->load->helper('form');
+			$this->load->helper('url');
+			$this->load->helper('html');
+			$this->load->database();
+			$this->load->library('form_validation');
 
-		foreach ( $this->mymodels as $m ) {
-			$this->load->model($m);
+			$this->mymodels = $_mymodels;
+
+			foreach ( $this->mymodels as $m ) {
+				$this->load->model($m);
+			}
+
+			$this->pdata = array( 'title'=>'');
 		}
-
-		/*
-        $this->uid = $this->session->userdata('uid');
-        if ( $this->uid != false ) {
-            $this->username = $this->session->userdata('username');
-		} else $this->username = ""; */
-        $this->pdata = array( 'title'=>'');//,"uid"=>$this->uid,"username"=>$this->username);
 
     }
 
     public function index() {
-        $bod  = "";
-        $bod .= "<div>Select a model to manually test</div>";
-        $bod .= "<div class='row well'>";
-		foreach( $this->mymodels as $m ) {
-			$bod .= "<div class='col-md-3 well'>";
-			$bod .= "  <div class='lead'><a href='/index.php/".get_class($this)."/model/".$m."'>".$m."</a></div>";
-			$bod .= "  <div><a href='/index.php/test/Test_".$m."' class='btn btn-default'>run unit tests</a></div>";
+		if ( $this->isvalid ) {
+			$bod  = "";
+			$bod .= "<div style='width:90%; margin: 10px;'>A CodeIgniter interactive model tester web interface. Directly call your model functionality for testing and debugging.</div>";
+			$bod .= "<div class='row well'>";
+			foreach( $this->mymodels as $m ) {
+				$bod .= "<div class='col-md-3 well'>";
+				$bod .= "  <div class='lead'><a href='/index.php/".get_class($this)."/model/".$m."'>".$m."</a></div>";
+				$bod .= "  <div><a href='/index.php/test/Test_".$m."' class='btn btn-default'>run unit tests</a></div>";
+				$bod .= "</div>";
+			}
 			$bod .= "</div>";
-		}
-        $bod .= "</div>";
-        $this->pdata["title"] = 'Model Tester';
-        $this->pdata["body"] = $bod;
-        //$this->template->load("default.php",NULL,$this->pdata);
+		} else {
+			$bod = "<div>Your ModelTester controller is shutting down because your CodeIgniter project is in a non development state.</div>"; }
+		$this->pdata["title"] = 'Model Tester';
+		$this->pdata["body"] = $bod;
 		echo $this->applyTemplate();
 
     }
 
     public function model_test() {
-        log_message("debug","model_test called with post data:");
-        log_message("debug",print_r($_POST,true));
-        //$ret = $this->adder_model->$fn();
-        // push all params into $pss array
-        $pss = array();
-        $fail = false;
-        $fn= $_POST['fn'];
-        $model = $_POST['model'];
-        foreach($_POST as $k=>$v) {
-            log_message("debug","check POST for param ".$k." ".$v);
-            // add params based upon order
-            if ( preg_match("/param(\d+)/",$k,$m) ) { 
-                $vv = json_decode($v,true);
-                log_message("debug","   json as ".print_r($vv,true));
-                if ( $vv !== NULL ) $pss[$m[1]] = $vv;
-                else //$fail = true;
-                    $pss[$m[1]] = $v;
-            } 
-        }
-        log_message("debug","pss".print_r($pss,true));
-        if ( $fail ) echo "fail";
-        else {
-			ob_start();
-            $ret = call_user_func_array(array($this->$model,$fn),$pss);
-			log_message("debug","ret is ".print_r($ret,true));
-			$content = ob_get_clean();
-			ob_end_clean();
-			log_message("debug","content is ".$content);
-			if  ( $ret === FALSE ) {
-				log_message("debug","ret failed");
-				$ret = array( "msg"     => "ERROR: failed, see log file.");
-			} else if ( $content != "" ) {
-				log_message("debug","ERROR: there was output from your model: '".$content."'\n");
-				$ret = array( "msg"     => "ERROR: there was output in your model.",
-					          "content" => $content );
-			} else { 
-				log_message('debug','json is: "'.json_encode($ret).'"');
+		if ( $this->isvalid ) {
+			log_message("debug","model_test called with post data:");
+			log_message("debug",print_r($_POST,true));
+			$pss = array(); // push all params into $pss array
+			$fail = false;
+			$fn= $_POST['fn'];
+			$model = $_POST['model'];
+			foreach($_POST as $k=>$v) {
+				log_message("debug","check POST for param ".$k." ".$v);
+				// add params based upon order
+				if ( preg_match("/param(\d+)/",$k,$m) ) { 
+					$vv = json_decode($v,true);
+					log_message("debug","   json as ".print_r($vv,true));
+					if ( $vv !== NULL ) $pss[$m[1]] = $vv;
+					else //$fail = true;
+						$pss[$m[1]] = $v;
+				} 
 			}
-			echo json_encode($ret); 
-        }
+			log_message("debug","pss".print_r($pss,true));
+			if ( $fail ) echo "fail";
+			else {
+				ob_start();
+				$ret = call_user_func_array(array($this->$model,$fn),$pss);
+				log_message("debug","ret is ".print_r($ret,true));
+				$content = ob_get_clean();
+				ob_end_clean();
+				log_message("debug","content is ".$content);
+				if  ( $ret === FALSE ) {
+					log_message("debug","ret failed");
+					$ret = array( "msg"     => "ERROR: failed, see log file.");
+				} else if ( $content != "" ) {
+					log_message("debug","ERROR: there was output from your model: '".$content."'\n");
+					$ret = array( "msg"     => "ERROR: there was output in your model.",
+								  "content" => $content );
+				} else { 
+					log_message('debug','json is: "'.json_encode($ret).'"');
+				}
+				echo json_encode($ret); 
+			}
+		}
         exit(1);
     }
     public function model($_model)
     {
-        $rc = new ReflectionClass($_model);//'Adder_model');
-        $methods = $rc->getMethods();
-        //print_r($methods);
+		if ( $this->isvalid ) {
+			$rc = new ReflectionClass($_model);
+			$methods = $rc->getMethods();
+
+			// Add navigation
+			$mtext   =  "<div class='row well'>".
+						"  <a class='btn btn-info btn-xs' href='/index.php/".get_class($this)."'>&lt; back </a>".
+						"  <a href='/index.php/test/Test_".$_model."' class='btn btn-default btn-xs'>run unit tests</a>".
+						"  <div class='btn-group btn-group-xs' role='group' aria-label='Switch Models'>";
+			foreach($this->mymodels as $m ) {
+				$btnt = ($m == $_model? "btn-primary" : "btn-info");
+				$mtext .= "    <a class='btn ".$btnt."' href='/index.php/".get_class($this)."/model/".$m."'>".$m."</a>";
+			}
+			$mtext   .= "  </div>".
+						"</div>";
+
+			$mtext .= "<div class='row'>\n";
+			$mid = 1;
+			foreach ($methods as $m ) {
+				if ( ! $m->isConstructor() ) {
+					$mtext .= "\n<div class='col-md-6 well'>";
+					$mtext .= "<form id='form".$mid."'>";
+					$mtext .= "<p><small>".$rc->name."-></small></p>";
+					$mtext .= "<fieldset><legend>".$m->name."</legend>";
+					//$mtext .= "<div class='lead'>".$m->name."</div>";
+					$mtext .= "<input type='hidden' name='fn' value='".$m->name."' ></input>";
+					$mtext .= "<input type='hidden' name='model' value='".$_model."' ></input>";
+					$dc = $m->getDocComment();
+					if ( $dc !== FALSE ) { $mtext .= "<div><pre><code style='font-size: 60%; line-height: 0px;'>".$dc."</code></pre></div>"; }
+					//log_message("debug",print_r($m->getDocComment(),true));
+					$np=0;
+					$mtext .= "<dl class=''>";
+					$theurl = "/index.php/".get_class($this)."/model_test/";
+					foreach ( $m->getParameters() as $p ) {
+						$mtext .= "<dt>".$p->name."</dt><dd><input name='param".$np."' type='text'></input></dd>";
+						$np++;
+					}
+					$mtext .= "</dl>";
+					$mtext .= "<input name='btn_submit' type='submit' class='btn btn-default' value='Submit' />\n";
+					$mtext .= "<div class='hide' id='".$m->name."_output'><pre></pre></div>";
+					$mtext .= "<script>$('#form".$mid."').submit(function(_e) { ".
+							  "   var data = $(this).serializeArray(); ".
+							  "   callModelTest({'url':'".$theurl."','data' : data },'".$m->name."'); _e.preventDefault() });</script>\n";
+					$mtext .= "</fieldset>";
+					$mtext .= "</form>";
+					$mtext .= "</div>\n";
+
+					$mid++;
+				}
+			}
+
+			$mtext .= "</div>";
 
 
-		// Add navigation
-		$mtext   =  "<div class='row well'>".
-					"  <a class='btn btn-info btn-xs' href='/index.php/".get_class($this)."'>&lt; back </a>".
-					"  <a href='/index.php/test/Test_".$_model."' class='btn btn-default btn-xs'>run unit tests</a>".
-					"  <div class='btn-group btn-group-xs' role='group' aria-label='Switch Models'>";
-		foreach($this->mymodels as $m ) {
-			$btnt = ($m == $_model? "btn-primary" : "btn-info");
-			$mtext .= "    <a class='btn ".$btnt."' href='/index.php/".get_class($this)."/model/".$m."'>".$m."</a>";
-		}
-		$mtext   .= "  </div>".
-			        "</div>";
+		} else {
+			$bod = "<div>Your ModelTester controller is shutting down because your CodeIgniter project is in a non development state.</div>"; }
 
-        $mtext .= "<div class='row'>\n";
-        $mid = 1;
-        foreach ($methods as $m ) {
-            if ( ! $m->isConstructor() ) {
-                $mtext .= "\n<div class='col-md-6 well'>";
-                $mtext .= "<form id='form".$mid."'>";
-                $mtext .= "<p><small>".$rc->name."-></small></p>";
-                $mtext .= "<fieldset><legend>".$m->name."</legend>";
-                //$mtext .= "<div class='lead'>".$m->name."</div>";
-                $mtext .= "<input type='hidden' name='fn' value='".$m->name."' ></input>";
-                $mtext .= "<input type='hidden' name='model' value='".$_model."' ></input>";
-                $dc = $m->getDocComment();
-                if ( $dc !== FALSE ) { $mtext .= "<div><pre><code style='font-size: 60%; line-height: 0px;'>".$dc."</code></pre></div>"; }
-                //log_message("debug",print_r($m->getDocComment(),true));
-                $np=0;
-                $mtext .= "<dl class=''>";
-                $theurl = "/index.php/".get_class($this)."/model_test/";
-                foreach ( $m->getParameters() as $p ) {
-                    $mtext .= "<dt>".$p->name."</dt><dd><input name='param".$np."' type='text'></input></dd>";
-                    $np++;
-                }
-                $mtext .= "</dl>";
-                $mtext .= "<input name='btn_submit' type='submit' class='btn btn-default' value='Submit' />\n";
-                $mtext .= "<div class='hide' id='".$m->name."_output'><pre></pre></div>";
-                $mtext .= "<script>$('#form".$mid."').submit(function(_e) { ".
-                          "   var data = $(this).serializeArray(); ".
-                          "   callModelTest({'url':'".$theurl."','data' : data },'".$m->name."'); _e.preventDefault() });</script>\n";
-                $mtext .= "</fieldset>";
-                $mtext .= "</form>";
-                $mtext .= "</div>\n";
-
-                $mid++;
-            }
-        }
-
-        $mtext .= "</div>";
-
-        $this->pdata["title"] = $_model;//'Adder Model';
-        $this->pdata["body"] = $mtext;
-        //$this->pdata["jsfiles"] = array("model_tester.js");
+		$this->pdata["title"] = $_model;//'Adder Model';
+		$this->pdata["body"] = $mtext;
 		echo $this->applyTemplate();
-        //$this->template->load("default.php",NULL,$this->pdata);
-
     }
 
 	protected function applyTemplate() {
@@ -186,8 +186,9 @@ class ModelTester extends CI_Controller {
 			"</head>".
 			"<body>".
 			"<div class='container'>".
-			"	<div class='row'><div class='col-md-12'><h1>Model Tester: ".$this->pdata['title']."</div></h1> </div>".
+			"	<div class='row'><div class='col-md-12'><h1>Model Tester: ".$this->pdata['title']."</h1></div></div>".
 			"	".$this->pdata['body']."".
+			"   <div class='row well'><div class='col-md-12'><small>A <a href='http://conquestcreations.com'>Conquest Creations</a> product. Find the <a href='https://github.com/cwingrav/ModelTester'>Github repo for ModelTester here.</a> </small></div></div>".
 			"</body>".
 			"</html>";
 		return $retval;
