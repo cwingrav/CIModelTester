18f5820e0bb435f06d588d1eb6465c6cd83970c3
Chad Wingrave <cwingrav@Chadwicks-MBP.home>
2015-03-18 20:48:17 -0400

    Update.

--- modeltester.php
+++ modeltester.php
@@ -1,5 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+
 class ModelTester extends CI_Controller {
 
     public function __construct($_mymodels,$_isvalid = true) {
